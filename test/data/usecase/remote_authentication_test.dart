9b2de1766146da9d61f0187df15a87d7102e2fd1
Matheus Gondim <matheus-pg1@hotmail.com>
2022-02-15 11:58:33 -0300

    refactor: move production code to their own target

--- test/data/usecase/remote_authentication_test.dart
+++ test/data/usecase/remote_authentication_test.dart
@@ -1,29 +1,11 @@
 import 'package:faker/faker.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
 import 'package:flutter_tdd_clean_arch/domain/usecases/authentication.dart';
 
-class RemoteAuthentication {
-  final HttpClient httpClient;
-  final String url;
-
-  RemoteAuthentication({
-    @required this.httpClient,
-    @required this.url,
-  });
-
-  Future<void> auth(AuthenticationParams params) async {
-    final body = {'email': params.email, 'password': params.secret};
-    await httpClient.request(url: url, method: "post", body: body);
-  }
-}
-
-abstract class HttpClient {
-  Future<void> request(
-      {@required String url, @required String method, Map body});
-}
+import 'package:flutter_tdd_clean_arch/data/http/http.dart';
+import 'package:flutter_tdd_clean_arch/data/usecases/usecases.dart';
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
@@ -47,8 +29,9 @@ void main() {
     await sut.auth(params);
 
     verify(httpClient.request(
-        url: url,
-        method: "post",
-        body: {'email': params.email, 'password': params.secret}));
+      url: url,
+      method: "post",
+      body: {'email': params.email, 'password': params.secret},
+    ));
   });
 }

f5ac5b894aa2d5ca5b6292b0779365250e370642
matheus-gondim <matheus-pg1@hotmail.com>
2022-03-08 16:55:55 -0300

    ensure HttpAdapter throws UnauthorizedError on 401

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -93,7 +93,7 @@ void main() {
     });
 
     test('Should return BadRequestError if post returns 400', () async {
-      mockResponse(400);
+      mockResponse(400, body: '');
 
       final future = sut.request(url: url, method: 'post');
 
@@ -101,11 +101,19 @@ void main() {
     });
 
     test('Should return BadRequestError if post returns 400', () async {
-      mockResponse(400, body: '');
+      mockResponse(400);
 
       final future = sut.request(url: url, method: 'post');
 
       expect(future, throwsA(HttpError.badRequest));
+    });
+
+    test('Should return UnauthorizedError if post returns 401', () async {
+      mockResponse(401);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.unauthorized));
     });
 
     test('Should return InternalServerError if post returns 500', () async {

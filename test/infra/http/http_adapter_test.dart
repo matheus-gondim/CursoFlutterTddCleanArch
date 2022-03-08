4a87f8d192431ce26af92cd1cbf98c9738d95076
matheus-gondim <matheus-pg1@hotmail.com>
2022-03-08 13:02:49 -0300

    ensure HttpAdapter calls post with correct Body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
@@ -19,7 +21,7 @@ class HttpAdapter {
       'accept': 'application/json'
     };
 
-    await client.post(url, headers: headers);
+    await client.post(url, headers: headers, body: jsonEncode(body));
   }
 }
 
@@ -38,7 +40,11 @@ void main() {
 
   group('post', () {
     test('Should call post with correct values', () async {
-      await sut.request(url: url, method: 'post');
+      await sut.request(
+        url: url,
+        method: 'post',
+        body: {'any_key': 'any_value'},
+      );
 
       verify(client.post(
         url,
@@ -46,6 +52,7 @@ void main() {
           'content-type': 'application/json',
           'accept': 'application/json'
         },
+        body: '{"any_key":"any_value"}',
       ));
     });
   });

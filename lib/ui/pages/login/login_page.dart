354cc79f1fd50345bf07d09f4e79d664ca7c5a7d
matheus-gondim <matheus-pg1@hotmail.com>
2022-03-18 13:14:30 -0300

    ensure LoginPage presents error if email is invalid

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -22,16 +22,22 @@ class LoginPage extends StatelessWidget {
               child: Form(
                 child: Column(
                   children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        icon: Icon(
-                          Icons.email,
-                          color: Theme.of(context).primaryColorLight,
-                        ),
-                      ),
-                      onChanged: presenter.validateEmail,
-                      keyboardType: TextInputType.emailAddress,
+                    StreamBuilder<String>(
+                      stream: presenter.emailErrorStream,
+                      builder: (context, snapshot) {
+                        return TextFormField(
+                          decoration: InputDecoration(
+                            labelText: 'Email',
+                            icon: Icon(
+                              Icons.email,
+                              color: Theme.of(context).primaryColorLight,
+                            ),
+                            errorText: snapshot.data,
+                          ),
+                          onChanged: presenter.validateEmail,
+                          keyboardType: TextInputType.emailAddress,
+                        );
+                      },
                     ),
                     Padding(
                       padding: EdgeInsets.only(top: 8, bottom: 32),

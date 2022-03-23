2a7be35f5634f65c093f5ba5f0a49e42b023296e
matheus-gondim <matheus-pg1@hotmail.com>
2022-03-23 14:05:50 -0300

    ensure LoginPage close all streams on dispose

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -272,4 +272,15 @@ void main() {
       expect(find.text("main Error"), findsOneWidget);
     },
   );
+
+  testWidgets(
+    "Should close streams on dispose",
+    (WidgetTester tester) async {
+      await loadPage(tester);
+
+      addTearDown(() {
+        verify(presenter.dispose()).called(1);
+      });
+    },
+  );
 }

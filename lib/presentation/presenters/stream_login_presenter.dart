c6919ddc7b16f5e6676148f7576817ebad2eb9f9
matheus-gondim <matheus-pg1@hotmail.com>
2022-03-24 15:32:17 -0300

    ensure StreamLoginPresenter calls Authentication with correct values

--- lib/presentation/presenters/stream_login_presenter.dart
+++ lib/presentation/presenters/stream_login_presenter.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 
 import 'package:meta/meta.dart';
 
+import '../../domain/usecases/usecases.dart';
+
 import '../protocols/protocols.dart';
 
 class LoginState {
@@ -18,6 +20,7 @@ class LoginState {
 
 class StreamLoginPresenter {
   final Validation validation;
+  final Authentication authentication;
   final _controller = StreamController<LoginState>.broadcast();
   final _state = LoginState();
 
@@ -30,7 +33,10 @@ class StreamLoginPresenter {
   Stream<bool> get isFormValidStream =>
       _controller.stream.map((state) => state.isFormValid).distinct();
 
-  StreamLoginPresenter({@required this.validation});
+  StreamLoginPresenter({
+    @required this.validation,
+    @required this.authentication,
+  });
 
   void _update() => _controller.add(_state);
 
@@ -45,5 +51,11 @@ class StreamLoginPresenter {
     _state.passwordError =
         validation.validate(field: 'password', value: password);
     _update();
+  }
+
+  Future<void> auth() async {
+    await authentication.auth(
+      AuthenticationParams(email: _state.email, secret: _state.password),
+    );
   }
 }
--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -2,13 +2,18 @@ import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
+import 'package:flutter_clean_architecture/domain/usecases/usecases.dart';
+
 import 'package:flutter_clean_architecture/presentation/presenters/presenters.dart';
 import 'package:flutter_clean_architecture/presentation/protocols/protocols.dart';
 
 class ValidationSpy extends Mock implements Validation {}
 
+class AuthenticationSpy extends Mock implements Authentication {}
+
 void main() {
   StreamLoginPresenter sut;
+  AuthenticationSpy authentication;
   ValidationSpy validation;
   String email;
   String password;
@@ -23,7 +28,9 @@ void main() {
 
   setUp(() {
     validation = ValidationSpy();
-    sut = StreamLoginPresenter(validation: validation);
+    authentication = AuthenticationSpy();
+    sut = StreamLoginPresenter(
+        validation: validation, authentication: authentication);
     email = faker.internet.email();
     password = faker.internet.password();
     mockValidation();
@@ -108,4 +115,17 @@ void main() {
     await Future.delayed(Duration.zero);
     sut.validatePassword(password);
   });
+
+  test(
+    'Should call Authentication with correct values',
+    () async {
+      sut.validateEmail(email);
+      sut.validatePassword(password);
+      await sut.auth();
+
+      verify(authentication
+              .auth(AuthenticationParams(email: email, secret: password)))
+          .called(1);
+    },
+  );
 }

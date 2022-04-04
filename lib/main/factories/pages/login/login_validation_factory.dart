55ac85a3a6f9ac71471d508be633be70e59ce25a
matheus-gondim <matheus-pg1@hotmail.com>
2022-04-04 12:22:48 -0300

    ensure LoginValidationsFactory returns correct validations

--- lib/main/factories/pages/login/login_validation_factory.dart
+++ lib/main/factories/pages/login/login_validation_factory.dart
@@ -1,10 +1,15 @@
 import '../../../../presentation/protocols/validation.dart';
 import '../../../../valitation/validators/validators.dart';
+import '../../../../valitation/protocols/protocols.dart';
 
 Validation makeLoginValidation() {
-  return ValidationComposite([
+  return ValidationComposite(makeLoginValidations());
+}
+
+List<FieldValidation> makeLoginValidations() {
+  return [
     EmailValidation('email'),
     RequiredFieldValidation('email'),
     RequiredFieldValidation('password'),
-  ]);
+  ];
 }
---
+++ test/main/factories/pages/login/login_validation_factory_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_clean_architecture/valitation/validators/validators.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_clean_architecture/main/factories/factories.dart';
+
+void main() {
+  test('Should return the correct validations', () {
+    final validations = makeLoginValidations();
+
+    expect(validations, [
+      EmailValidation('email'),
+      RequiredFieldValidation('email'),
+      RequiredFieldValidation('password'),
+    ]);
+  });
+}

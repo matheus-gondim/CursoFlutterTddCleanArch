edf4b4810d1e4c757935a200d5a49b6e92262ced
matheus-gondim <matheus-pg1@hotmail.com>
2022-04-04 12:45:50 -0300

    hide keyboard on tap

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -15,6 +15,11 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  void _hideKeyboard() {
+    final currentFocus = FocusScope.of(context);
+    if (!currentFocus.hasPrimaryFocus) currentFocus.unfocus();
+  }
+
   @override
   void dispose() {
     super.dispose();
@@ -36,36 +41,39 @@ class _LoginPageState extends State<LoginPage> {
         if (error != null) showErrorMessage(context, error);
       });
 
-      return SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            LoginHeader(),
-            Headline1(text: 'Login'),
-            Padding(
-              padding: EdgeInsets.all(32),
-              child: Provider(
-                create: (_) => widget.presenter,
-                child: Form(
-                  child: Column(
-                    children: [
-                      EmailInput(),
-                      Padding(
-                        padding: EdgeInsets.only(top: 8, bottom: 32),
-                        child: PasswordInput(),
-                      ),
-                      LoginButton(),
-                      FlatButton.icon(
-                        onPressed: () {},
-                        icon: Icon(Icons.person),
-                        label: Text('Criar Conta'),
-                      )
-                    ],
+      return GestureDetector(
+        onTap: _hideKeyboard,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              LoginHeader(),
+              Headline1(text: 'Login'),
+              Padding(
+                padding: EdgeInsets.all(32),
+                child: Provider(
+                  create: (_) => widget.presenter,
+                  child: Form(
+                    child: Column(
+                      children: [
+                        EmailInput(),
+                        Padding(
+                          padding: EdgeInsets.only(top: 8, bottom: 32),
+                          child: PasswordInput(),
+                        ),
+                        LoginButton(),
+                        FlatButton.icon(
+                          onPressed: () {},
+                          icon: Icon(Icons.person),
+                          label: Text('Criar Conta'),
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       );
     }));

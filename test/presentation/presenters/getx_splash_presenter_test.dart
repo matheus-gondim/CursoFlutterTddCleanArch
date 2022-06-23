2e643525d1d8c83e2dbf7d6c39b1f4ec6daf7271
matheus-gondim <matheus-pg1@hotmail.com>
2022-06-23 10:47:47 -0300

    ensure SplashPresenter goes to login if token is null

--- test/presentation/presenters/getx_splash_presenter_test.dart
+++ test/presentation/presenters/getx_splash_presenter_test.dart
@@ -42,6 +42,13 @@ void main() {
     await sut.checkAccount(durantionInSeconds: 0);
   });
 
+  test('Should go to login page on null token', () async {
+    mockLoadCurrentAccount(account: AccountEntity(null));
+
+    sut.navigateToStream.listen(expectAsync1((page) => expect(page, '/login')));
+    await sut.checkAccount(durantionInSeconds: 0);
+  });
+
   test('Should go to login page on null result', () async {
     mockLoadCurrentAccount(account: null);
 

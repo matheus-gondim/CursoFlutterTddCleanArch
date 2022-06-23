84c32908f7576d1272e9a3cf2213e0ff989ad51e
matheus-gondim <matheus-pg1@hotmail.com>
2022-06-23 13:21:21 -0300

    move RemoteLoadSurveys to its own file

--- test/data/usecase/load_surveys/remote_load_surveys_test.dart
+++ test/data/usecase/load_surveys/remote_load_surveys_test.dart
@@ -1,4 +1,3 @@
-import 'package:meta/meta.dart';
 import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
@@ -7,27 +6,7 @@ import 'package:flutter_clean_architecture/domain/entities/entities.dart';
 import 'package:flutter_clean_architecture/domain/helpers/helpers.dart';
 
 import 'package:flutter_clean_architecture/data/http/http.dart';
-import 'package:flutter_clean_architecture/data/models/models.dart';
-
-class RemoteLoadSurveys {
-  final HttpClient<List<Map>> httpClient;
-  final String url;
-
-  RemoteLoadSurveys({@required this.httpClient, @required this.url});
-
-  Future<List<SurveyEntity>> load() async {
-    try {
-      final httpResponse = await httpClient.request(url: url, method: 'get');
-      return httpResponse
-          .map((json) => RemoteSurveyModel.fromJson(json).toEntity())
-          .toList();
-    } on HttpError catch (error) {
-      throw error == HttpError.forbidden
-          ? DomainError.accessDenied
-          : DomainError.unexpected;
-    }
-  }
-}
+import 'package:flutter_clean_architecture/data/usecases/usecases.dart';
 
 class HttpClientSky extends Mock implements HttpClient<List<Map>> {}
 

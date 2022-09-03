4d72c858af6647f609436975afb87d8f85f54640
matheus-gondim <matheus-pg1@hotmail.com>
2022-09-03 14:58:20 -0300

    ensure LocalLoadSurveys returns a list of surveys on success

---
+++ lib/data/models/local_survey_model.dart
@@ -0,0 +1,38 @@
+import 'package:meta/meta.dart';
+
+import '../../domain/entities/entities.dart';
+
+import '../http/http.dart';
+
+class LocalSurveyModel {
+  final String id;
+  final String question;
+  final DateTime date;
+  final bool didAnswer;
+
+  LocalSurveyModel({
+    @required this.id,
+    @required this.question,
+    @required this.date,
+    @required this.didAnswer,
+  });
+
+  factory LocalSurveyModel.fromJson(Map json) {
+    if (!json.keys.toSet().containsAll(['id', 'date', 'question', 'didAnswer']))
+      throw HttpError.invalidData;
+
+    return LocalSurveyModel(
+      id: json['id'],
+      question: json['question'],
+      date: DateTime.parse(json['date']),
+      didAnswer: json['didAnswer'].toString().toLowerCase() == 'true',
+    );
+  }
+
+  SurveyEntity toEntity() => SurveyEntity(
+        id: id,
+        question: question,
+        didAnswer: didAnswer,
+        dateTime: date,
+      );
+}
